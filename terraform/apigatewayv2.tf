03d916570c2341dbd72b139e71fb0b2a20a30886
Philip Fasan <40202619+phil-f@users.noreply.github.com>
2023-10-31 00:18:56 +0000

    cleanup

--- terraform/apigatewayv2.tf
+++ terraform/apigatewayv2.tf
@@ -74,3 +74,13 @@ resource "aws_apigatewayv2_route" "update_weather" {
   authorizer_id = aws_apigatewayv2_authorizer.weather.id
   target    = "integrations/${aws_apigatewayv2_integration.update_weather.id}"
 }
+
+resource "aws_apigatewayv2_authorizer" "weather" {
+  api_id                            = aws_apigatewayv2_api.weather.id
+  authorizer_type                   = "REQUEST"
+  authorizer_uri                    = aws_lambda_function.weather_authorizer.invoke_arn
+  identity_sources                  = ["$request.header.Authorization"]
+  name                              = "${var.resource_prefix}weather-authorizer"
+  authorizer_payload_format_version = "2.0"
+  enable_simple_responses           = true
+}
--- terraform/authorizer.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_apigatewayv2_authorizer" "weather" {
-  api_id                            = aws_apigatewayv2_api.weather.id
-  authorizer_type                   = "REQUEST"
-  authorizer_uri                    = aws_lambda_function.weather_authorizer.invoke_arn
-  identity_sources                  = ["$request.header.Authorization"]
-  name                              = "${var.resource_prefix}weather-authorizer"
-  authorizer_payload_format_version = "2.0"
-  enable_simple_responses           = true
-}
